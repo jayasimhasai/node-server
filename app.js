e5409a09fda0bd349863f075c675ec440eb210af
sai <jaisimhasai@gmail.com>
2018-05-04 15:22:19 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const hbs =require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname+'/views/partials');
+
+app.use((req,res,next)=>{
+	var now = new Date().toString();
+	var log = `${now} ${req.method} ${req.url}`;
+	console.log(log);
+	fs.appendFile('server.log',log+'\n',(err)=>{
+		if(err){
+			console.log('error while appending');
+		}
+		
+	});
+	next();
+});
+
+hbs.registerHelper('getCurrentYear' , ()=>{
+	return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text)=>{
+	return text.toUpperCase();
+});
+
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname+'/public'));
+app.get('/',(req,res)=>{
+	//res.send('<h1>Hello welcome to sai server<h1>');
+	res.render('template.hbs',{
+		pageTitle:"Home",
+	});
+});
+app.get('/bad',(req,res)=>{
+	res.send({
+		error_message:"unable to handle request"
+	});
+})
+app.get('/about',(req,res)=>{
+	res.render('template.hbs',{
+		pageTitle: "About",
+	});
+});
+app.listen(3000);
